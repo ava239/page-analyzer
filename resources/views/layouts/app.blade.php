7ee59aebd3799a469eeb65968c1f78da19318b30
ava <ava239@yandex.ru>
2020-09-08 22:13:44 +0300

    changed app title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="csrf-param" content="_token"/>
 
-    <title>Laravel</title>
+    <title>Page Analyzer</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
