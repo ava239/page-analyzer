ff1162a7dd0c8624ccb09b4d0c8c75b932a6bbf1
ava <ava239@yandex.ru>
2020-09-12 00:20:10 +0300

    queues for domain checks

--- app/Http/Controllers/DomainCheckController.php
+++ app/Http/Controllers/DomainCheckController.php
@@ -2,40 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use DiDom\Document;
-use Illuminate\Http\Client\ConnectionException;
+use App\Jobs\ProcessDomainCheck;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Http;
 
 class DomainCheckController extends Controller
 {
     public function store($id)
     {
         $domain = DB::table('domains')->where('id', $id)->first();
-        if (!$domain) {
-            abort(404);
-        }
-        try {
-            $response = Http::get($domain->name);
-        } catch (ConnectionException $exception) {
-            flash('Check error. Url could not be resolved')->error();
-            return back();
-        }
-        $dom = new Document();
-        if ($response->body()) {
-            $dom->loadHtml($response->body());
-        }
-        $domainCheckData = [
-            'domain_id' => $id,
-            'status_code' => $response->status(),
-            'h1' => optional($dom->first('h1'))->text(),
-            'keywords' => optional($dom->first('meta[name="keywords"]'))->content,
-            'description' => optional($dom->first('meta[name="description"]'))->content,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ];
-        DB::table('domain_checks')->insert($domainCheckData);
-        flash('Website has been checked!')->success();
+        abort_unless($domain !== null, 404);
+        ProcessDomainCheck::dispatch($domain);
         return redirect()->route('domains.show', $id);
     }
 }
---
+++ app/Jobs/ProcessDomainCheck.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Jobs;
+
+use DiDom\Document;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+
+class ProcessDomainCheck implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    private object $domain;
+
+    public function __construct($domain)
+    {
+        $this->domain = $domain;
+    }
+
+    public function handle()
+    {
+        try {
+            $response = Http::get($this->domain->name);
+        } catch (ConnectionException $exception) {
+            flash("Check error. Could not resolve '{$this->domain->name}'")->error();
+            return;
+        }
+        $dom = new Document();
+        if ($response->body()) {
+            $dom->loadHtml($response->body());
+        }
+        $domainCheckData = [
+            'domain_id' => $this->domain->id,
+            'status_code' => $response->status(),
+            'h1' => optional($dom->first('h1'))->text(),
+            'keywords' => optional($dom->first('meta[name="keywords"]'))->content,
+            'description' => optional($dom->first('meta[name="description"]'))->content,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+        DB::table('domain_checks')->insert($domainCheckData);
+        flash("Website '{$this->domain->name}' has been checked!")->success();
+    }
+}
